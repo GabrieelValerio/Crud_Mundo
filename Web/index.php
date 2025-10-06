8ffe1229ec0195f137941af85c254a0e3bd9178d
Gabriel Valerio <gabrielvalerio2929@gmail.com>
2025-10-06 11:05:33 -0300

    Update index.php(06/10/2025 - 11:04)
    Colocando os "botões" para excluir e editar nos países e cidades.

--- Web/index.php
+++ Web/index.php
@@ -1,5 +1,5 @@
 <?php
-    include 'bd.php'; //conexão ao banco de dados
+    include 'bd.php'; // conexão ao banco de dados
 ?>
 
 <!DOCTYPE html>
@@ -9,68 +9,75 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud Mundo</title>
+    <link rel="shortcut icon" href="../Assets/icon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>SEJA BEM VINDO AO CRUD MUNDO!</h1>
+    <h1>SEJA BEM-VINDO AO CRUD MUNDO!</h1>
     <h2>GERENCIE AQUI O MUNDO</h2>
 
+    <div class="container">
 
-    <!--TABELAS-->
-
-    <table>
-        <tr>
-            <th>Continente</th>
-            <th>Nome</th>
-            <th>População</th>
-            <th>Idioma</th>
-        </tr>
-
-        <?php
-            $sql = "select * from tb_pais";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<tr>
-                        <td>".$row['continente']."</td>
-                        <td>".$row['nome_oficial']."</td>
-                        <td>".$row['populacao']."</td>
-                        <td>".$row['idioma_principal']."</td>
-                    </tr>";
+        <!-- TABELA DE PAÍSES -->
+        <table>
+            <tr>
+                <th>Continente</th>
+                <th>Nome</th>
+                <th>População</th>
+                <th>Idioma</th>
+            </tr>
+            <?php
+                $sql = "SELECT * FROM tb_pais";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tr>
+                                <td>".$row['continente']."</td>
+                                <td>".$row['nome_oficial']."</td>
+                                <td>".$row['populacao']."</td>
+                                <td>".$row['idioma_principal']."</td>
+                                <td>
+                                    <a href='editar_pais.php?id=".$row['id_pais']."' class='btn editar'>Editar</a>
+                                    <a href='excluir_pais.php?id=".$row['id_pais']."' class='btn excluir'>Excluir</a>
+                                </td>
+                              </tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='4'>Nenhum país encontrado!</td></tr>";
                 }
-            } else{
-                echo "<tr><td>Nenhum país encontrado! </td></tr>";
-            }
-        ?>
+            ?>
+        </table>
 
-    </table>
-
-    <table>
-        <tr>
-            <th>País</th>
-            <th>Cidade</th>
-            <th>População</th>
-        </tr>
-
-        <?php
-            $sql = "select p.nome_oficial as pais, c.nome_oficial, c.populacao from tb_cidade c inner join tb_pais p on (c.id_pais=p.id_pais)";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<tr>
-                        <td>".$row['pais']."</td>
-                        <td>".$row['nome_oficial']."</td>
-                        <td>".$row['populacao']."</td>
-                    </tr>";
+        <!-- TABELA DE CIDADES -->
+        <table>
+            <tr>
+                <th>País</th>
+                <th>Cidade</th>
+                <th>População</th>
+            </tr>
+            <?php
+                $sql = "SELECT id_cidade, p.nome_oficial AS pais, c.nome_oficial AS cidade, c.populacao 
+                        FROM tb_cidade c 
+                        INNER JOIN tb_pais p ON c.id_pais = p.id_pais";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tr>
+                                <td>".$row['pais']."</td>
+                                <td>".$row['cidade']."</td>
+                                <td>".$row['populacao']."</td>
+                                <td>
+                                    <a href='editar_cidade.php?id=".$row['id_cidade']."' class='btn editar'>Editar</a>
+                                    <a href='backend/excluir_cidade.php?id=".$row['id_cidade']."' class='btn excluir'>Excluir</a>
+                                </td>
+                              </tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>Nenhuma cidade encontrada!</td></tr>";
                 }
-            } else{
-                echo "<tr><td>Nenhuma cidade encontrada! </td></tr>";
-            }
-        ?>
+            ?>
+        </table>
 
-    </table>
-
+    </div>
 </body>
 </html>
